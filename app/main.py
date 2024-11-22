336e41ccd52966bb389fe8e1fd133644c13c543b
Gaurav R <gaurav.r@innovalabs.tech>
2024-11-22 16:24:50 +0530

    encript password

--- app/main.py
+++ app/main.py
@@ -8,6 +8,8 @@ from app.database import engine, SessionLocal
 import app.models as models
 from sqlalchemy.orm import Session
 
+from passlib.context import CryptContext
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -55,11 +57,24 @@ def read_users(db: db_dependency):
 
 @app.post("/users", response_model=UserBase)
 def create_user(user: UserCreate, db: db_dependency):
-    db_user = models.User(name=user.name, email=user.email, role=user.role, hash_password=user.password)
+    hash_password = bcrypt_context.hash(user.password)
+    db_user = models.User(name=user.name, email=user.email, role=user.role, hash_password=hash_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@app.patch("/users/{user_id}/change_password")
+def change_password(user_id: int, old_password: str, new_password: str, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not bcrypt_context.verify(old_password, user.hash_password):
+        raise HTTPException(status_code=401, detail="Incorrect old_password")
+    user.hash_password = bcrypt_context.hash(new_password)
+    db.commit()
+    db.refresh(user)
+    return {"message": "Password changed successfully"}
 
 @app.get("/campaigns", response_model=List[CampaignBase])
 def read_campaigns(db: db_dependency):
