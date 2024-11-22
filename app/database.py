d129dc97f083f6fde98932bafd04d247c24380db
Gaurav R <gaurav.r@innovalabs.tech>
2024-11-22 16:58:33 +0530

    add .env file

--- app/database.py
+++ app/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-URL_DATABASE="postgresql://gr:gr@localhost:5432/p1"
+URL_DATABASE = os.getenv("DATABASE_URL")
+
+if not URL_DATABASE:
+    raise ValueError("DATABASE_URL is not set in the environment variables.")
 
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
